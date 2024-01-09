bd1d764df0e2202f306392e9d01e9d2c9d53a865
May Shech <may745@gmail.com>
2024-01-09 15:55:52 +0200

    Added footer,header and main to dashboard , still not finished

---
+++ src/app/dashboard/footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+    return (
+        <footer className="bg-white flex-shrink-0">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="border-t border-gray-200 py-8 text-sm text-gray-500 text-center sm:text-left">
+                    <span className="block sm:inline">&copy; 2024 Your Company</span> <span className="block sm:inline">All rights reserved.</span>
+                </div>
+            </div>
+        </footer>
+    );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,24 @@
+import Footer from "./footer";
+import Header from "./header";
+
 export default function Dashbaord() {
-  return <h1>Dashbaord</h1>;
+  return (
+    <div className="flex flex-col min-h-screen">
+    <Header />
+    <main className="flex-1">
+      {/* Replace with your content */}
+      <div className="bg-white py-6">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          {/* Replace with your content */}
+          <div className="py-4">
+            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+          </div>
+          {/* /End replace */}
+        </div>
+      </div>
+      {/* /End replace */}
+    </main>
+    <Footer />
+</div>
+  )
 }
