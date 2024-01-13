d766477382658c5c9a0a74bf8469592079e0d494
May Shech <may745@gmail.com>
2024-01-13 23:40:13 +0200

    Design dashboard landing page (#3)
    * Added new fields in task menu
    
    * Added mb-2 to input
    
    * Started to make the db save but got into some defficulties need you with me, only one time so i can do this alone
    
    * Minor Fixes
    
    * Added validation for future dates only
    
    * Looks better now
    
    * Minor fixes
    
    * Deleted on submit for now , need to configure the function first
    
    * fix

--- src/components/Modal/CreateTaskModal.tsx
+++ src/components/Modal/CreateTaskModal.tsx
@@ -1,6 +1,7 @@
 import ModalWrapper from './ModalWrapper';
 import BaseForm from '../BaseForm';
-import { useForm } from 'react-hook-form';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { useState } from 'react';
 import FormInput from '../Input/FormInput';
 import { emailRegex } from '../../consts';
 
@@ -8,20 +9,42 @@ type Props = {
   show: boolean;
   onClose: () => void;
 };
+
 const CreateTaskModal: React.FC<Props> = ({ show, onClose }) => {
   const {
     handleSubmit,
     register,
     formState: { errors, isValid, isDirty },
   } = useForm();
+
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    if (!isValid) return;
+    setIsSubmitting(true);
+
+    try {
+    } catch (error) {
+      console.error('Error in create task: ', error);
+    }
+  };
+
   return (
     <ModalWrapper show={show} onBackdrop={onClose}>
-      <BaseForm title="Create Task" onCancel={onClose} btnSize="md">
+      <BaseForm
+        title="Create Task"
+        onCancel={onClose}
+        btnSize="md"
+        submitText="Create"
+        onSubmit={handleSubmit(onSubmit)}
+        disabled={!isValid && isDirty}
+        isSubmitting={isSubmitting}
+      >
         {/* TODO: refactor inputs for task creation fields fields */}
         <FormInput
-          title="Email"
+          title="Assignee"
           type="text"
-          placeholder="Email"
+          inputClassName={'mb-2'}
+          placeholder="Assignee email"
           isInline={false}
           errorMessage={errors.email?.message}
           {...register('email', {
@@ -29,6 +52,33 @@ const CreateTaskModal: React.FC<Props> = ({ show, onClose }) => {
             pattern: {
               value: emailRegex,
               message: 'Invalid email format',
+            },
+          })}
+        />
+        <FormInput
+          title="Description"
+          type="text"
+          inputClassName={'mb-2 h-20'}
+          placeholder="Description"
+          isInline={false}
+          errorMessage={errors.description?.message}
+          {...register('description', {
+            required: 'Description Name is required',
+          })}
+        />
+        <FormInput
+          title="Due Date"
+          type="date"
+          inputClassName={'mb-2'}
+          placeholder="Due Date"
+          isInline={false}
+          errorMessage={errors.dueDate?.message}
+          {...register('dueDate', {
+            required: 'Due Date is required',
+            validate: (value) => {
+              const today = new Date();
+              const dueDate = new Date(value);
+              return dueDate > today || 'Due Date must be in the future';
             },
           })}
         />
