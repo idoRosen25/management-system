25711b7c8421b9f2ee4f1d60e5f120bca0c3504b
May Shech <may745@gmail.com>
2024-01-20 23:55:16 +0200

    Added params via context

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -2,10 +2,10 @@ import { NextRequest, NextResponse } from 'next/server';
 import prisma from '../../../../../lib/prismadb';
 import exp from 'constants';
 
-export async function PUT(request: NextRequest) {
- const id  = request.url.split('/').slice(-1)[0];
+export async function PUT(request: NextRequest, context: { params: any }) {
+  const id = context.params.id;
   const { title, description, assigneeEmail } = await request.json();
-  const task = await prisma.$transaction(async ()=>{
+  const task = await prisma.$transaction(async () => {
     const task = await prisma.task.update({
       where: { id },
       data: {
@@ -30,21 +30,20 @@ export async function PUT(request: NextRequest) {
   return NextResponse.json(task);
 }
 
-
-export async function DELETE(request: NextRequest) {
+export async function DELETE(request: NextRequest, context: { params: any }) {
   try {
-    const id = request.url.split('/').slice(-1)[0];
+    const id = context.params.id;
     await prisma.assignedTask.deleteMany({ where: { taskId: id } });
     await prisma.task.delete({ where: { id } });
-    return NextResponse.json("task deleted");
+    return NextResponse.json('task deleted');
   } catch (error: any) {
     return NextResponse.json({ error: error?.toString() }, { status: 500 });
   }
 }
 
-export async function GET(request: NextRequest) {
+export async function GET(request: NextRequest, context: { params: any }) {
   try {
-    const id = request.url.split('/').slice(-1)[0];
+    const id = context.params.id;
     const task = await prisma.task.findUnique({
       where: { id },
       include: { assignedTo: true },
--- src/components/Modal/EditTaskModal.tsx
+++ src/components/Modal/EditTaskModal.tsx
@@ -1,100 +1,111 @@
-import {axios} from "../../utils/axios";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { Endpoints } from "@/consts";
-import { createTaskSchema } from "@/schema/task";
-import { zodResolver } from "@hookform/resolvers/zod";
-import BaseForm from "../BaseForm";
-import ModalWrapper from "./ModalWrapper";
-import FormInput from "../Input/FormInput";
-import z from "zod";
-
+import { axios } from '../../utils/axios';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Endpoints } from '@/consts';
+import { createTaskSchema } from '@/schema/task';
+import { zodResolver } from '@hookform/resolvers/zod';
+import BaseForm from '../BaseForm';
+import ModalWrapper from './ModalWrapper';
+import FormInput from '../Input/FormInput';
+import { twMerge } from 'tailwind-merge';
+import z from 'zod';
 
 type Props = {
-    show: boolean;
-    onClose: () => void; // Add onClose prop
-    taskID: string;
-    };
+  show: boolean;
+  onClose: () => void; // Add onClose prop
+  taskID: string;
+};
 
 type FormData = z.infer<typeof createTaskSchema>;
 
 const EditTaskModal: React.FC<Props> = ({ show, onClose, taskID }) => {
-    const router = useRouter();
-    const {
-        handleSubmit,
-        register,
-        reset,
-        formState: { errors, isValid, isDirty },
-    } = useForm<FormData>({
-        resolver: zodResolver(createTaskSchema),
-    });
+  const router = useRouter();
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors, isValid, isDirty },
+  } = useForm<FormData>({
+    resolver: zodResolver(createTaskSchema),
+  });
 
-    const handleClose = () => {
-        onClose();
-        reset();
-    };
+  const handleClose = () => {
+    onClose();
+    reset();
+  };
 
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const onSubmit: SubmitHandler<FormData> = async (data) => {
-        if (!isValid) return;
-        setIsSubmitting(true);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    if (!isValid) return;
+    setIsSubmitting(true);
 
-        try {
-            const response = await axios.put(
-                Endpoints.TASKS + '/' + taskID,
-                JSON.stringify({ ...data }),
-                {
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                },
-            );
-            if (!response.data) {
-                throw new Error('Invalid credentials');
-            }
+    try {
+      const response = await axios.put(
+        Endpoints.TASKS + '/' + taskID,
+        JSON.stringify({ ...data }),
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        },
+      );
+      if (!response.data) {
+        throw new Error('Invalid credentials');
+      }
 
-            handleClose();
-            router.refresh();
-        } catch (error) {
-            console.error('Error in create task: ', error);
-        }
-    };
+      handleClose();
+      router.refresh();
+    } catch (error) {
+      console.error('Error in create task: ', error);
+    }
+  };
 
-    return (
-        <ModalWrapper show={show} onBackdrop={handleClose}>
-            <BaseForm
-                title="Edit task"
-                onSubmit={handleSubmit(onSubmit)}
-                submitText="Edit task"
-                isSubmitting={isSubmitting}
-                disabled={!isDirty || !isValid}
-            >
-                <FormInput
-                    title="Title"
-                    placeholder="Title"
-                    type="text"
-                    {...register('title')}
-                    error={errors?.title?.message}
-                />
-                <FormInput
-                    title="Description"
-                    placeholder="Description"
-                    type="text"
-                    {...register('description')}
-                    error={errors?.description?.message}
-                />
-                <FormInput
-                    title="Assignee"
-                    placeholder="Assignee"
-                    type="text"
-                    {...register('assigneeEmail')}
-                    error={errors?.assigneeEmail?.message}
-                />
-            </BaseForm>
-        </ModalWrapper>
-    );
+  return (
+    <ModalWrapper show={show} onBackdrop={handleClose}>
+      <BaseForm
+        title="Edit task"
+        onSubmit={handleSubmit(onSubmit)}
+        submitText="Edit task"
+        isSubmitting={isSubmitting}
+        disabled={!isDirty || !isValid}
+      >
+        <FormInput
+          title="Title"
+          placeholder="Title"
+          type="text"
+          {...register('title')}
+          error={errors?.title?.message}
+        />
+        <FormInput
+          title="Description"
+          placeholder="Description"
+          type="text"
+          customInput={
+            <textarea
+              className={twMerge(
+                'border border-gray-400 rounded-md pl-2 w-[54%]',
+                errors.description?.message ? 'border-2 border-red-600' : '',
+              )}
+              placeholder="Description"
+              {...register('description', {
+                required: 'Description is required',
+              })}
+            />
+          }
+          {...register('description')}
+          error={errors?.description?.message}
+        />
+        <FormInput
+          title="Assignee"
+          placeholder="Assignee"
+          type="text"
+          {...register('assigneeEmail')}
+          error={errors?.assigneeEmail?.message}
+        />
+      </BaseForm>
+    </ModalWrapper>
+  );
 };
-
 
 export default EditTaskModal;
