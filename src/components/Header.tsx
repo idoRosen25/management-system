8e01081add066dd89bb18880a574e55a745ed095
Ido Rosenblum <idorosen25@gmail.com>
2024-01-20 18:06:13 +0200

    fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import CreateTaskButton from '@/components/Button/CreateTaskButton';
 export default function Header() {
   return (
     <nav
-      className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8 bg-blue-400 h-[80px]"
+      className="mx-auto flex w-full items-center justify-between p-6 lg:px-8 bg-blue-400 h-[80px]"
       aria-label="Global"
     >
       <div className="flex lg:flex-1"></div>
