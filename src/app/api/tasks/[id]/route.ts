c4ce4402b84d18b7e33764d7dc91d2525a9523a3
May Shech <may745@gmail.com>
2024-01-21 20:09:59 +0200

    Use the assigneeEmail from the task in the where clause. Also add check if new assigneeEmail is different from the current task.assigneeEmail before updating

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -1,28 +1,33 @@
 import { NextRequest, NextResponse } from 'next/server';
 import prisma from '../../../../../lib/prismadb';
 
-
-export async function PUT(request: NextRequest, context: { params: NodeJS.Dict<string>  }) {
+export async function PUT(
+  request: NextRequest,
+  context: { params: NodeJS.Dict<string> },
+) {
   const id = context.params.id;
   const { title, description, assigneeEmail } = await request.json();
   const task = await prisma.$transaction(async () => {
-    const task = await prisma.task.update({
-      where: { id },
-      data: {
-        title,
-        description,
-      },
-    });
-    try {
-      assigneeEmail &&
-        (await prisma.assignedTask.update({
-          where: { taskId_assigneeEmail: { taskId: task.id, assigneeEmail } },
+    try{
+      const task = await prisma.task.findUnique({
+        where: { id },
+        include: { assignedTo: true },
+      });
+      if (!task) throw new Error('Task not found');
+        await prisma.task.update({
+          where: { id },
           data: {
-            assigneeEmail,
-            taskId: task.id,
+            title,
+            description,
           },
-        }));
-      return task;
+        });
+        if (assigneeEmail && assigneeEmail !== task.assignedTo?.assigneeEmail) {
+          await prisma.assignedTask.update({
+            where: { taskId: id },
+            data: { assigneeEmail },
+          });
+        } 
+        return task;
     } catch (error) {
       throw error;
     }
@@ -30,7 +35,10 @@ export async function PUT(request: NextRequest, context: { params: NodeJS.Dict<s
   return NextResponse.json(task);
 }
 
-export async function DELETE(request: NextRequest, context: { params: NodeJS.Dict<string> }) {
+export async function DELETE(
+  request: NextRequest,
+  context: { params: NodeJS.Dict<string> },
+) {
   try {
     const id = context.params.id;
     await prisma.assignedTask.deleteMany({ where: { taskId: id } });
@@ -41,7 +49,10 @@ export async function DELETE(request: NextRequest, context: { params: NodeJS.Dic
   }
 }
 
-export async function GET(request: NextRequest, context: { params: NodeJS.Dict<string> }) {
+export async function GET(
+  request: NextRequest,
+  context: { params: NodeJS.Dict<string> },
+) {
   try {
     const id = context.params.id;
     const task = await prisma.task.findUnique({
