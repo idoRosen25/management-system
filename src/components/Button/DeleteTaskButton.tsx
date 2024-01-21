96d3767ff20e411edad65a5a6002b1dfa50cdcbe
May Shech <may745@gmail.com>
2024-01-21 18:44:40 +0200

    Added loading

--- src/components/Button/DeleteTaskButton.tsx
+++ src/components/Button/DeleteTaskButton.tsx
@@ -3,19 +3,26 @@ import { Endpoints } from "@/consts";
 import Button from "./Button";
 import { axios } from "@/utils/axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 
 const DeleteTaskButton = ({ taskId }: { taskId: string }) => {
     const router = useRouter();
+    const [loading, setLoading] = useState(false);
     const deleteTask = async () => {
+        setLoading(true);
         await axios.delete(Endpoints.TASKS + '/' + taskId);
+        setLoading(false);
         router.refresh();
     };
 
     return (
+        
         <Button
         text={'Delete task'}
         color="danger"
         onClick={() => deleteTask()}
+        isLoading={loading}
         />
     );
 }
