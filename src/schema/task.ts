639f4286eeeedf972f480b94969bd911fbf34746
May Shech <may745@gmail.com>
2024-01-21 18:47:51 +0200

    Added new schema

--- src/schema/task.ts
+++ src/schema/task.ts
@@ -10,3 +10,13 @@ export const createTaskSchema = z.object({
   status: z.nativeEnum(TaskStatus).optional(),
   assigneeEmail: z.string().optional(),
 });
+
+export const updateTaskSchema = z.object({
+  title: z.string({ required_error: 'Title cannot be empty' }).min(5).max(1000).optional(),
+  description: z
+    .string({ required_error: 'Description cannot be empty' })
+    .min(2)
+    .max(4000).optional(),
+  status: z.nativeEnum(TaskStatus).optional(),
+  assigneeEmail: z.string().optional(),
+});
