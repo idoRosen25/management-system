e0045580ad77dc42ff6550bcc043238da3c33bce
May Shech <may745@gmail.com>
2024-01-22 18:00:25 +0200

    Added new Loading component

--- src/app/dashboard/@task/[id]/loading.tsx
+++ src/app/dashboard/@task/[id]/loading.tsx
@@ -1,8 +1,8 @@
 export default function Loading() {
   return (
     <div className="absolute top-0 left-0 w-screen h-screen bg-white bg-opacity-50 z-10 flex justify-center items-center">
-      <div className="border border-black rounded-lg m-auto w-max flex justify-center items-center">
-        <h1> this is the modal loading state in id folder</h1>
+      <div className="flex space-x-2 justify-center items-center h-screen dark:invert">
+      <div className="rounded-md h-12 w-12 border-4 border-t-4 border-blue-500 animate-spin absolute"></div>
       </div>
     </div>
   );
