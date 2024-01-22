95d2896c0c17a9335be5863260e49e1c2a70e9e6
May Shech <may745@gmail.com>
2024-01-22 17:57:34 +0200

    Added a new menu (tell me what you think the design i will change later)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -22,6 +22,89 @@ const Sidebar = () => {
         />
         <span className="text-lg font-medium">OctoManage</span>
       </LinkWrapper>
+
+      <div
+        className="
+      border-b border-black w-full h-0.5 my-1.5"
+      />
+
+      <div className="flex flex-col gap-2">
+        <LinkWrapper href={Routes.USER} className="flex items-center gap-2 p-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-gray-900"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path fill="#fff" d="M12 14l9-5-9-5-9 5 9 5z" />
+            <path
+              fill="#fff"
+              d="M12 14l9-5-9-5-9 5 9 5z"
+              stroke="#374151"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              fill="#fff"
+              d="M12 14l9-5-9-5-9 5 9 5z"
+              stroke="#374151"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              fill="#fff"
+              d="M12 14l9-5-9-5-9 5 9 5z"
+              stroke="#374151"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <span className="text-lg font-medium">Profile</span>
+        </LinkWrapper>
+        <LinkWrapper
+          href={Routes.DASHBOARD}
+          className="flex items-center gap-2 p-4"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 text-gray-900"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path fill="#fff" d="M12 14l9-5-9-5-9 5 9 5z" />
+            <path
+              fill="#fff"
+              d="M12 14l9-5-9-5-9 5 9 5z"
+              stroke="#374151"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              fill="#fff"
+              d="M12 14l9-5-9-5-9 5 9 5z"
+              stroke="#374151"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              fill="#fff"
+              d="M12 14l9-5-9-5-9 5 9 5z"
+              stroke="#374151"
+              strokeWidth={2}
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          <span className="text-lg font-medium">Dashboard</span>
+        </LinkWrapper>
+      </div>
     </div>
   );
 };
