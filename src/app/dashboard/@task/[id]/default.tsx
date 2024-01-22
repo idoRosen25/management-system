d17566b1a0d83c00c4a6157528ab2e695f895e35
Ido Rosenblum <idorosen25@gmail.com>
2024-01-22 12:14:32 +0200

    fix tasks display

--- src/app/dashboard/@task/[id]/default.tsx
+++
@@ -1,7 +0,0 @@
-export default function Default() {
-  <div className="absolute top-0 left-0 w-screen h-screen bg-white bg-opacity-50 z-10 flex justify-center items-center">
-    <div className="border border-black rounded-lg m-auto w-max flex justify-center items-center">
-      <h1>this is the single task modal default state</h1>
-    </div>
-  </div>;
-}
