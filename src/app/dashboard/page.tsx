8cf29ab31058921062cc2c39402b42cdf8f24feb
May Shech <may745@gmail.com>
2024-01-22 17:58:07 +0200

    Mapped the TaskStatus to be in every column

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,11 +6,16 @@ import TaskColumnLoader from '../../components/Tasks/TaskColumnLoader';
 export default function Dashbaord() {
   return (
     <section>
-      {/* Replace with your content */}
       <div className="bg-white py-6">
         <div className="w-full mx-auto px-4 sm:px-6 lg:px-8">
-          {/* Replace with your content */}
-          <div className="grid grid-cols-3 gap-6 w-3/4">
+          <div className="w-full text-2xl font-extralight text-gray-900 text-center gap-6 grid grid-cols-3 mb-3 underline">
+            {Object.values(TaskStatus).map((status, index) => (
+              <div key={`task_status_${status}`}>
+                {status.replaceAll('_', ' ')}
+              </div>
+            ))}
+          </div>
+          <div className="grid grid-cols-3 gap-6">
             {Object.values(TaskStatus).map((status, index) => (
               <Suspense
                 key={`suspence_task_status_${status}`}
