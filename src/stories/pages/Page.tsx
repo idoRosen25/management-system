6699be9a07c629c9c6239b30b10f71c3f2b5cdd3
Ido Rosenblum <idorosen25@gmail.com>
2024-01-24 16:24:53 +0200

    fix page component for storybook

--- src/stories/pages/Page.tsx
+++ src/stories/pages/Page.tsx
@@ -22,14 +22,14 @@ export const Page: React.FC = () => {
       <section className="storybook-page">
         <h2>Pages in Storybook</h2>
         <p>
-          We recommend building UIs with a{' '}
+          We recommend building UIs with a
           <a
             href="https://componentdriven.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             <strong>component-driven</strong>
-          </a>{' '}
+          </a>
           process starting with atomic components and ending with pages.
         </p>
         <p>
@@ -40,7 +40,7 @@ export const Page: React.FC = () => {
         <ul>
           <li>
             Use a higher-level connected component. Storybook helps you compose
-            such data from the "args" of child component stories
+            such data from the args of child component stories
           </li>
           <li>
             Assemble data in the page component from your services. You can mock
@@ -48,7 +48,7 @@ export const Page: React.FC = () => {
           </li>
         </ul>
         <p>
-          Get a guided tutorial on component-driven development at{' '}
+          Get a guided tutorial on component-driven development at
           <a
             href="https://storybook.js.org/tutorials/"
             target="_blank"
@@ -56,7 +56,7 @@ export const Page: React.FC = () => {
           >
             Storybook tutorials
           </a>
-          . Read more in the{' '}
+          . Read more in the
           <a
             href="https://storybook.js.org/docs"
             target="_blank"
@@ -68,7 +68,7 @@ export const Page: React.FC = () => {
         </p>
         <div className="tip-wrapper">
           <span className="tip">Tip</span> Adjust the width of the canvas with
-          the{' '}
+          the
           <svg
             width="10"
             height="10"
