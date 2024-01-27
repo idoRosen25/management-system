01e4d4371b4fe50d4eca1d6af34aba276ed84649
May Shech <may745@gmail.com>
2024-01-27 23:29:38 +0200

    SAB

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: '.env.local' });
+dotenv.config({ path: '.env.local' || '.env' });
 
 /**
  * Read environment variables from file.
