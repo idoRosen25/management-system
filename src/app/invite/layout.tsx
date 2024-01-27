86d5b08923b87d1af406311e6980b9f7b6c3626b
May Shech <may745@gmail.com>
2024-01-27 18:39:26 +0200

    added a few minor improvments tell me if you like
    git push
    
    exot
    exit

--- src/app/invite/layout.tsx
+++ src/app/invite/layout.tsx
@@ -12,17 +12,29 @@ export default async function Layout(props: {
   const user = await getLoggedInUser();
 
   return (
-    <main className="w-screen h-screen flex gap-4 bg-indigo-700 bg-opacity-80 items-center justify-center">
-      {!user || (user && user.role) ? (
-        props.children
-      ) : user.role === Role.ADMIN ? (
-        <div className="w-full h-full divide-x-1 flex flex-col lg:flex-row">
-          {props.teams}
-          {!!user?.teamId && (
-            <>
-              <div className="w-full h-2 lg:h-full lg:w-1 bg-gray-700" />
-              {props.user}
-            </>
+    <div className="flex overflow-y-hidden no-scrollbar">
+      <Sidebar route={Routes.INVITE} />
+      <div className="w-full">
+        <Header />
+        <main className="w-screen h-screen flex gap-4 bg-blue-700 bg-opacity-60 items-center justify-center border-black border-l">
+          {!user ? (
+            props.children
+          ) : user.role === Role.ADMIN ? (
+            <div className="w-full h-full divide-x-1 flex flex-col lg:flex-row">
+              {props.teams}
+              {!!user?.teamId && (
+                <>
+                  <div className="w-full h-2 lg:h-full lg:w-1 bg-gray-700" />
+                  {props.user}
+                </>
+              )}
+            </div>
+          ) : user.role === Role.OWNER ? (
+            props.user
+          ) : (
+            <span className="bg-white rounded-lg p-4">
+              Only Admins and Owners can invite to team.
+            </span>
           )}
         </main>
       </div>
