b4a012bf851020ed6d20db9ee2fc7e147e782a75
May Shech <may745@gmail.com>
2024-02-02 17:33:29 +0200

    Improvments

--- src/app/invite/layout.tsx
+++ src/app/invite/layout.tsx
@@ -12,32 +12,40 @@ export default async function Layout(props: {
   const user = getLoggedInUser();
 
   return (
-    <div className="flex overflow-y-hidden no-scrollbar">
-      <Sidebar route={Routes.INVITE} />
-      <div className="w-full">
-        <Header />
-        <main className="w-screen h-screen flex gap-4 bg-blue-700 bg-opacity-60 items-center justify-center border-black border-l">
-          {!user ? (
-            props.children
-          ) : user.role === Role.ADMIN ? (
-            <div className="w-full h-full divide-x-1 flex flex-col lg:flex-row">
-              {props.teams}
-              {!!user?.teamId && (
-                <>
-                  <div className="w-full h-2 lg:h-full lg:w-1 bg-gray-700" />
-                  {props.user}
-                </>
+    <div>
+      {user ? (
+        <div className="flex overflow-y-hidden no-scrollbar">
+          <Sidebar route={Routes.INVITE} />
+          <div className="w-full">
+            <Header />
+            <main className="w-full h-full flex gap-4 bg-blue-700 bg-opacity-60 items-center justify-center border-black border-l">
+              {!user ? (
+                props.children
+              ) : user.role === Role.ADMIN ? (
+                <div className="w-full h-full divide-x-1 flex flex-col lg:flex-row">
+                  {props.teams}
+                  {!!user?.teamId && (
+                    <>
+                      <div className="w-full h-2 lg:h-full lg:w-1 bg-gray-700" />
+                      {props.user}
+                    </>
+                  )}
+                </div>
+              ) : user.role === Role.OWNER ? (
+                props.user
+              ) : (
+                <span className="bg-white rounded-lg p-4">
+                  Only Admins and Owners can invite to team.
+                </span>
               )}
-            </div>
-          ) : user.role === Role.OWNER ? (
-            props.user
-          ) : (
-            <span className="bg-white rounded-lg p-4">
-              Only Admins and Owners can invite to team.
-            </span>
-          )}
-        </main>
-      </div>
+            </main>
+          </div>
+        </div>
+      ) : (
+        <div className="w-screen h-screen flex items-center justify-center bg-blue-700 bg-opacity-70">
+          {props.children}
+        </div>
+      )}
     </div>
   );
 }
