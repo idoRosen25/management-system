30e5e4ac2b6389a060ba07c04b119a51de8a8de5
May Shech <may745@gmail.com>
2024-02-05 15:33:31 +0200

    new test

--- tests/Auth/auth.spec.ts
+++ tests/Auth/auth.spec.ts
@@ -52,14 +52,14 @@ test('create static user in db', async () => {
   });
 });
 
-// test('login test without signup', async ({ page }) => {
-//   // Expect a title "to contain" a substring.
-//   await page.getByPlaceholder('Email').fill(User.email);
-//   await page.getByPlaceholder('Password').fill(User.password);
-//   await page.getByRole('button', { name: 'Log in' }).click();
-//   await page.waitForURL(Routes.DASHBOARD);
-//   expect(page.url()).toBe(baseURL + Routes.DASHBOARD);
-//   expect(await page.getByRole('button', { name: 'Logout' }).textContent()).toBe(
-//     'Logout',
-//   );
-// });
+test('login test without signup', async ({ page }) => {
+  // Expect a title "to contain" a substring.
+  await page.getByPlaceholder('Email').fill(User.email);
+  await page.getByPlaceholder('Password').fill(User.password);
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.waitForURL(Routes.DASHBOARD);
+  expect(page.url()).toBe(baseURL + Routes.DASHBOARD);
+  expect(await page.getByRole('button', { name: 'Logout' }).textContent()).toBe(
+    'Logout',
+  );
+});
